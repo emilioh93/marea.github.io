abfa71bf8e1a13f26c1c36dbe40c345f8be71697
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-03-23 10:55:28 -0300

    agregué app js para modiicar enlace de whatsapp al ser mobile

--- js/app.js
+++ js/app.js
@@ -1,7 +1,4 @@
-// Detectar mobile
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-    // Cambio link
-    document.querySelector("#whatsapp").href = "https://api.whatsapp.com/send?phone=543816241279&text=Hola%20Marea%20Gestión,%20 necesito%20información";
-} else {
-    document.querySelector("#whatsapp").href = "https://web.whatsapp.com/send?phone=543816241279&text=Hola%20Marea%20Gestión,%20 necesito%20información";
-}
+    // Detectar mobile
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.querySelector(".whatsappEnlace").href = "https://api.whatsapp.com/send?phone=543816241279&text=Hola%20Marea%20Gestión,%20necesito%20información";
+    }
