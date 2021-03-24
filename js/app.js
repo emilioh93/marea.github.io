fd098a950ecfb961b73ea4367524f909e4d1e3d0
Emilio Hurtado <emiliohurtado1@gmail.com>
2021-03-24 11:27:52 -0300

    agregué librería owl carrousel

--- js/app.js
+++ js/app.js
@@ -66,3 +66,22 @@
         });
 
     }
+
+
+    // CAROUSEL
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 4
+            },
+            1000: {
+                items: 4
+            }
+        }
+    })
